5ba8adde3ff77676149f4d6aafc1b9b4790bebc5
putraperdana138 <putraperdana138@students.unnes.ac.id>
2022-05-25 19:22:42 +0700

    Add react hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useMemo } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [loading, setLoading] = useState(false);
+	const [products, setProducts] = useState([]);
+
+	const datas = useMemo(
+		() => [
+			{
+				id: 1,
+				nama: "Ari",
+				alamat: "Batam",
+			},
+			{
+				id: 2,
+				nama: "Budi",
+				alamat: "Jakarta",
+			},
+			{
+				id: 3,
+				nama: "Ani",
+				alamat: "Semarang",
+			},
+		],
+		[loading]
+	);
+
+	const renderData = () => {
+		setProducts([
+			{
+				id: 1,
+				nama: "Buku",
+				quantity: 15,
+			},
+		]);
+	};
+
+	useEffect(() => renderData, [loading]);
+
+	return (
+		<div className="App">
+			<button type="button" onClick={() => setLoading(!loading)}>
+				Test
+			</button>
+			<table>
+				<tr>
+					<td>No</td>
+					<td>Nama</td>
+					<td>Alamat</td>
+				</tr>
+				{!loading &&
+					datas.map((v) => (
+						<tr>
+							<td>{v.id}</td>
+							<td>{v.nama}</td>
+							<td>{v.alamat}</td>
+						</tr>
+					))}
+			</table>
+
+			<table>
+				<tr>
+					<td>No</td>
+					<td>Nama Barang</td>
+					<td>Jumlah</td>
+				</tr>
+				{!loading &&
+					products.map((v) => (
+						<tr>
+							<td>{v.id}</td>
+							<td>{v.nama}</td>
+							<td>{v.quantity}</td>
+						</tr>
+					))}
+			</table>
+		</div>
+	);
 }
 
 export default App;
